5eea7f4b246f77f0a629eac8d498e9409dea9f86
Freddy R. Laffita Almaguer <c3s4rfred@gmail.com>
2022-12-27 15:27:50 -0500

    Adding default ThreadPoolExecutor with 10 threads for entity batch executions

--- src/main/java/com/sdk/threatwinds/factory/RequestFactory.java
+++ src/main/java/com/sdk/threatwinds/factory/RequestFactory.java
@@ -2,22 +2,32 @@ package com.sdk.threatwinds.factory;
 
 import com.sdk.threatwinds.factory.request.TWEndpointRequest;
 import com.sdk.threatwinds.interfaces.IRequestExecutor;
+import com.sdk.threatwinds.service.UtilitiesService;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
 /*
-* Main class of the SDK, dedicated to define the request to
-* be executed
-* */
+ * Main class of the SDK, dedicated to define the request to
+ * be executed, if you don't provide a ThreadPoolExecutor defaults to
+ * 10 concurrent threads
+ * */
 public class RequestFactory {
     int batchSize;
     ThreadPoolExecutor executor;
-    public RequestFactory(int batchSize, ThreadPoolExecutor executor) {
+
+    public RequestFactory(int batchSize) {
         this.batchSize = batchSize;
-        this.executor = executor;
     }
 
-    public IRequestExecutor getExecutor (){
-            return new TWEndpointRequest(this.batchSize, this.executor);
+    public IRequestExecutor getExecutor() {
+        if (this.executor == null) {
+            this.executor = UtilitiesService.defaultThreadPoolExecutor;
         }
+        return new TWEndpointRequest(this.batchSize, this.executor);
+    }
+
+    public RequestFactory withThreadPoolExecutor(ThreadPoolExecutor executor) {
+        this.executor = executor;
+        return this;
+    }
 }
